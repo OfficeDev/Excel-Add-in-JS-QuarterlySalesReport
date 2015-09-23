8764f25ebba5c37db7fbcf5b5230c72075fe6486
Uma Subramanian <umas@microsoft.com>
2015-09-23 11:19:31 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,68 @@
+# Quarterly Sales Report Task Pane Add-in Sample for Excel 2016
+
+_Applies to: Excel 2016_
+
+This is a simple Excel task pane add-in that loads some data into a worksheet and creates a basic chart in Excel 2016. It comes in two flavors: text editor and Visual Studio.
+
+![Quarterly Sales Report Sample](images/QuarterlySalesReport_report.png)
+
+## Try it out
+### Text editor version
+
+The simplest way to deploy and test your add-in is to copy the files to a network share.
+
+1.  Create a folder on a network share (for example, \\MyShare\QuarterlySalesReport) and copy all the files in the Text Editor folder. 
+2.  Edit the <SourceLocation> element of the manifest file so that it points to the share location from step 1. 
+3.  Copy the manifest (QuarterlySalesReportManifest.xml) to a network share (for example, \\MyShare\MyManifests).
+4.  Add the share location that contains the manifest as a trusted app catalog in Excel.
+
+    a.  Launch Excel and open a blank spreadsheet.  
+    
+    b.  Choose the **File** tab, and then choose **Options**.
+    
+    c.  Choose **Trust Center**, and then choose the **Trust Center Settings** button.
+    
+    d.  Choose **Trusted App Catalogs**.
+    
+    e.  In the **Catalog Url** box, enter the path to the network share you created in step 1, and then choose **Add Catalog**.
+    
+   f.  Select the **Show in Menu** check box, and then choose **OK**. A message appears to inform you that your settings will be applied the next time you start Office. 
+        
+5.  Test and run the add-in. 
+
+    a.  In the **Insert tab** in Excel 2016, choose **My Add-ins**. 
+    
+    b.  In the **Office Add-ins** dialog box, choose **Shared Folder**.
+    
+    c.  Choose **Quarterly Sales Report Sample**>**Insert**. The add-in opens in a task pane to the right of the current worksheet, as shown in the following figure. 
+        
+  ![Quarterly Sales Report Sample](images/QuarterlySalesReport_taskpane.PNG))
+
+    d.  Click the **Click me!** button to render the data and the chart inside the worksheet, as shown in the following figure.  To see the chart update dynamically, just change the data in the range. 
+        
+  ![Quarterly Sales Report Sample](images/QuarterlySalesReport_report.png)
+
+### Visual Studio version
+1.  Copy the project to a local folder and open the Excel-Add-in-JS-QuarterlySalesReport.sln in Visual Studio.
+2.  Press F5 to build and deploy the sample add-in. Excel launches and the add-in opens in a task pane to the right of a blank worksheet, as shown in the following figure. 
+        
+  ![Quarterly Sales Report Sample](images/QuarterlySalesReport_taskpane.PNG)
+
+3. Click the **Click me!** button to render the data and the chart inside the worksheet, as shown in the following figure.  To see the chart update dynamically, just change the data in the range. 
+        
+  ![Quarterly Sales Report Sample](images/QuarterlySalesReport_report.png)
+        
+## Code it
+
+If you want to write this add-in yourself from scratch, see [Build your first Excel Add-in](https://github.com/OfficeDev/office-js-docs/blob/master/excel/build-your-first-excel-add-in.md).
+
+
+### Learn more
+
+The Excel JavaScript APIs have much more to offer you as you develop add-ins. The following are just a few of the available resources. 
+
+1.  [Excel Add-ins programming overview](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-programming-overview.md)
+2.  [Snippet Explorer for Excel](http://officesnippetexplorer.azurewebsites.net/#/snippets/excel)
+3.  [Excel Add-ins code samples](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-code-samples.md) 
+4.  [Excel Add-ins JavaScript API Reference](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-javascript-reference.md)
+5.  [Build your first Excel Add-in](https://github.com/OfficeDev/office-js-docs/blob/master/excel/build-your-first-excel-add-in.md)
---
+++ Visual Studio/Excel-Add-in-JS-QuarterlySalesReportWeb/App/Home/Home.js
@@ -0,0 +1,101 @@
+﻿/* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+    See full license at the bottom of this file. */
+
+/// <reference path="../App.js" />
+
+(function () {
+	"use strict";
+
+	// The initialize function must be run each time a new page is loaded
+	Office.initialize = function (reason) {
+		$(document).ready(function () {
+			app.initialize();
+
+			$('#load-data-and-create-chart').click(loadDataAndCreateChart);
+		});
+	};
+
+	// Load some sample data into the worksheet and then create a chart
+	function loadDataAndCreateChart() {
+		// Run a batch operation against the Excel object model
+		Excel.run(function (ctx) {
+
+			// Create a proxy object for the active worksheet
+			var sheet = ctx.workbook.worksheets.getActiveWorksheet();
+
+			//Queue commands to set the report title in the worksheet
+			sheet.getRange("A1").values = "Quarterly Sales Report";
+			sheet.getRange("A1").format.font.name = "Century";
+			sheet.getRange("A1").format.font.size = 26;
+
+			//Create an array containing sample data
+			var values = [["Product", "Qtr1", "Qtr2", "Qtr3", "Qtr4"],
+						  ["Frames", 5000, 7000, 6544, 4377],
+						  ["Saddles", 400, 323, 276, 651],
+						  ["Brake levers", 12000, 8766, 8456, 9812],
+						  ["Chains", 1550, 1088, 692, 853],
+						  ["Mirrors", 225, 600, 923, 544],
+						  ["Spokes", 6005, 7634, 4589, 8765]];
+
+			//Queue a command to write the sample data to the specified range
+			//in the worksheet and bold the header row
+			var range = sheet.getRange("A2:E8");
+			range.values = values;
+			sheet.getRange("A2:E2").format.font.bold = true;
+
+			//Queue a command to add a new chart
+			var chart = sheet.charts.add("ColumnClustered", range, "auto");
+
+			//Queue commands to set the properties and format the chart
+			chart.setPosition("G1", "L10");
+			chart.title.text = "Quarterly sales chart";
+			chart.legend.position = "right"
+			chart.legend.format.fill.setSolidColor("white");
+			chart.dataLabels.format.font.size = 15;
+			chart.dataLabels.format.font.color = "black";
+			var points = chart.series.getItemAt(0).points;
+			points.getItemAt(0).format.fill.setSolidColor("pink");
+			points.getItemAt(1).format.fill.setSolidColor('indigo');
+
+			//Run the queued-up commands, and return a promise to indicate task completion
+			return ctx.sync();
+		})
+		  .then(function () {
+			  app.showNotification("Success");
+			  console.log("Success!");
+		  })
+		.catch(function (error) {
+			// Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+			app.showNotification("Error: " + error);
+			console.log("Error: " + error);
+			if (error instanceof OfficeExtension.Error) {
+				console.log("Debug info: " + JSON.stringify(error.debugInfo));
+			}
+		});
+	}
+})();
+
+/*
+Excel-Add-in-JS-QuarterlySalesReportManifest, https://github.com/OfficeDev/Excel-Add-in-JS-QuarterlySalesReport
+
+Copyright (c) Microsoft Corporation
+
+All rights reserved. 
+
+MIT License:
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, including 
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the 
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT 
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
